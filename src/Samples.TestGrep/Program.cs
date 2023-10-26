23c6a463b066564ad22f13432e2096a67eed9cb5
Martin Ortega <lynxestudio@outlook.com>
2023-10-26 00:41:46 -0600

    Add source code to project

---
+++ src/Samples.TestGrep/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace Samples.TestGrep
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			if(args.Length == 2)
+			{
+    			string pattern = args[0];
+ 				string file = args[1];
+ 				Regex regexp = new Regex(@pattern,RegexOptions.Multiline);
+ 				FileInfo fi = new FileInfo(file);
+ 			if(File.Exists(fi.FullName))
+ 			{
+  			using(StreamReader sr = new StreamReader(file))
+  			{
+   				string line;
+   				while((line = sr.ReadLine())!= null)
+   				{
+    				Match m = regexp.Match(line);
+    				if(m.Success)
+    				Console.WriteLine("{0}",line);
+   				}
+  			}
+ 			}
+ 			else
+  				Console.WriteLine("File not found");
+			}
+			else
+			Console.WriteLine("Usage: mono TestGrep [pattern] [file]");
+		}
+	}
+}
